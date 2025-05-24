03a6abd7d1e923ab3bb45dff05f61aedb93ae995
richie <richiepagard@gmail.com>
2025-05-25 01:00:32 +0330

    feat: Complete the `main` function in python logging user logs.
    - Get inputs from user and handle them in the `main` function.
    
    - Handle the possible exceptions in the `main` function.
    
    - Call the `user` function in `main` and log it.
    
    - Logs user information after calling the `user` function.

--- python-logging/user-input/log_user_input.py
+++ python-logging/user-input/log_user_input.py
@@ -28,7 +28,48 @@ def user(first_name: str, last_name: str, username: str) -> str:
 
 
 def main():
+    """
+    Main function will run the program when call it.
+
+    Handle exceptions and inputs. Logs the occured exceptions and other program parts.
+    """
+
     logging.info("Program started.")
+
+    fname_input = lname_input = username_input = "Unknown"
+
+    try:
+        fname_input = input("Enter your first name please: ")
+        lname_input = input("Enter your last name please: ")
+        username_input = input("Enter your username please: ")
+
+        logging.info("Received user information.")
+        logging.info("-" * 40)
+
+    except EOFError as e:
+        logging.exception("End of file error. No input received.")
+        logging.info("-" * 40)
+        # Re-raise the cought exception
+        raise
+
+    except Exception as e:
+        logging.exception("An unexpected error occured: {e}".format(e))
+        logging.info("-" * 40)
+        # Re-raise the cought exception
+        raise
+
+    # Check inputs if each of them is NULL, then set it as a 'Unknown'
+    first_name = fname_input.title() or "Unknown"
+    last_name = lname_input.title() or "Unknown"
+    username = username_input.lower() or "Unknown"
+
+    # Logs the `user` function
+    logging.info(user(first_name, last_name, username))
+
+    logging.info(
+        "User with \"{}\" firstname, \"{}\" lastname, and \"{}\" username has just logged in.".format(first_name, last_name, username)
+    )
+    logging.info("-" * 40)
 
 
 if __name__ == '__main__':
