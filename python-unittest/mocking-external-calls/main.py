86966778513019a3f127d170a51ad098d3dc913a
richie <richiepagard@gmail.com>
2025-06-01 01:27:40 +0330

    feat: Start experimenting mocking in unittest. Just write a program yet.
    - Write a program to get a URL and send the status code simply.
    
    - Implement a function to get a URL, send request to it and return the status code.

---
+++ python-unittest/mocking-external-calls/main.py
@@ -0,0 +1,26 @@
+"""
+This program simply gets a URL from the user input,
+and checks its status code. For instance, status code: 200
+
+Methods:
+    check_url_status_code (output: int): Gets a URL from its arguments,
+    send request to it and then return the status code.
+"""
+
+import requests
+
+
+def check_url_status_code(url: str) -> int:
+    """
+    Takes a URL and returns its status code.
+
+    Arguments:
+        url (str): The URL to send a request to.
+    
+    Returns:
+        status code (int): The status code of the given url.
+    """
+    page = requests.get(url)
+    page_status_code = page.status_code
+
+    return page_status_code
