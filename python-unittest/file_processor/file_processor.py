bfe37f8e3155d5710289f88b5b0561504f19afa8
richie <richiepagard@gmail.com>
2025-06-17 01:49:55 +0330

    feat: Implement the basic features of `file processor` function such as get the file name and read the file. `python-unittest/file_processor/file_processor.py`.

--- python-unittest/file_processor/file_processor.py
+++ python-unittest/file_processor/file_processor.py
@@ -2,3 +2,21 @@
 A File Processor Program.
 Reads a text file and shows the number of lines, words, and characters.
 """
+from pathlib import Path
+
+FILE_NAME = input("Enter the file name to read: ")
+
+# Ensure the file exists
+Path(FILE_NAME).touch(exist_ok=True)
+
+
+def file_processor(file_name: str) -> str:
+    content = None
+
+    with open(file_name, 'r') as file:
+        content = file.read()
+
+    return content
+
+
+print(file_processor(FILE_NAME))
