ac048330f3694e212a7b38ccfbdf719ca4016ae4
richie <richiepagard@gmail.com>
2025-07-05 02:19:34 +0330

    test: Add new unit test for a file with no content.

--- python-unittest/file_processor/tests/test_file_processor.py
+++ python-unittest/file_processor/tests/test_file_processor.py
@@ -19,18 +19,36 @@ class TestFileProcessor(unittest.TestCase):
     Methods:
         test_file_with_simple_content(self): Tests the file_processor function with a simple file content.
     """
+    def setUp(self):
+        """
+        Prepares the test fixtures.
+        Define 'line_count', 'word_count', and 'char_count' variables.
+        """
+        self.line_count = self.word_count = self.char_count = 0
+
     def test_file_with_simple_content(self):
         # Create a temporary file with simple content
         with tempfile.NamedTemporaryFile(mode='w+', delete=True, encoding='utf-8') as temp_file:
             temp_file.write("Hello World\nThis is a simple content for a test file to test the file processor program.")
             temp_file.seek(0)
 
-            line_count, word_count, char_count = file_processor(temp_file.name)
+            self.line_count, self.word_count, self.char_count = file_processor(temp_file.name)
 
-            self.assertEqual(line_count, 1)
-            self.assertEqual(word_count, 17)
+            self.assertEqual(self.line_count, 1)
+            self.assertEqual(self.word_count, 17)
             # Only ascii letters counted
-            self.assertEqual(char_count, 71)
+            self.assertEqual(self.char_count, 71)
+
+    def test_file_with_no_content(self):
+        with tempfile.NamedTemporaryFile(mode='w+', delete=True, encoding='utf-8') as temp_file:
+            temp_file.write("")
+            temp_file.seek(0)
+
+            self.line_count, self.word_count, self.char_count = file_processor(temp_file.name)
+
+            self.assertEqual(self.line_count, 0)
+            self.assertEqual(self.word_count, 0)
+            self.assertEqual(self.char_count, 0)
 
 
 if __name__ == '__main__':
