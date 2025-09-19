5e721a98d8c0244dfa8762a6b5dcf0d313e1bc84
MartyBin <m.hosseini9998@gmail.com>
2025-09-19 13:14:59 +0330

    feat: add new feature for `get_subdomain.py`

--- string-wizard/get-url_subdomain/get_subdomain.py
+++ string-wizard/get-url_subdomain/get_subdomain.py
@@ -5,13 +5,17 @@ url = input('Enter your url here: ')
 def subdomain_extractor(url: str) -> str | None:
     if url.startswith('http'):
         # Extract the subdomain using regex
-        match = re.search(r'(?<=https://).*', url)
-        if match:
-            subdomain = match.group(1).split('.')[0]
-            return subdomain
-        else:
-            return None
+        match1 = re.search(r'(?<=https://).*', url)
+        match2 = re.search(r'(?<=http://).*', url)
 
+        if match1 or match1:
+            subdomain = match1.group(0).split('.')[0]
+            return subdomain
+            
+        elif match1 or match2:
+            subdomain = match2.group(0).split('.')[0]
+            return subdomain
+            
 
 if __name__ == '__main__':
     subdomain = subdomain_extractor(url)
